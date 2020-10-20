69358b6d4e9cfa6d69168f7bcd2f74d9ccf04e82
uncle-yura <uncle-yura@tuta.io>
2020-10-20 22:53:01 +0300

    Added main files.

--- README.md
+++ README.md
@@ -1,2 +1,302 @@
 # esphome-midea-ac
 Monitoring and controlling Midea-like air conditioners
+
+## Features
+* Instant feedback of command changes via RF Remote to HomeAssistant.
+* Direct control without the remote.
+
+## Supported Microcontrollers
+This library should work on most ESP8266 or ESP32 platforms. It has been tested
+with the following MCUs:
+* Generic ESP-12S board (ESP8266)
+
+## Usage
+### Step 1: Build a control circuit.
+
+The signal levels are TTL 5V which means a level shifter is required when using 
+the ESP-01 to convert the signals from 5V to 3.3V and vice versa. Also a voltage
+regulator is required if you want to power the ESP-01 from the 5V of the UART 
+port.
+
+Some AC units provide a JST-XH male connector to connect the WiFi SmartKey 
+others provide a Type-A USB female connector (often connected to a JST-XH male 
+connector) to connect the WiFi SmartKey. So you either need a Type-A USB male 
+connector or JST-XH female connector to connect the ESP-01.
+
+The drawing and table show which pins need to be connected.
+
+![](./img/adapter.png)
+
+| ESP-01 adapter | USB | JST-XH |
+|-|-|-|
+| Gnd | 4 | 4 |
+| Vcc | 1 | 1 |
+| Rx | 2 | 2 |
+| Tx | 3 | 3 |
+
+![](./img/USB.jpeg)
+![](./img/JST-XH.jpeg)
+
+### Step 2: Clone this repository into your ESPHome configuration directory
+
+This repository needs to live in your ESPHome configuration directory, as it
+doesn't work correctly when used as a Platform.IO library, and there doesn't
+seem to be an analog for that functionality for ESPHome code.
+
+On Hass.IO, you'll want to do something like:
+
+* Change directories to your esphome configuration directory.
+* `cd config/esphome`
+* `git clone https://github.com/uncle-yura/esphome-midea-ac.git`
+
+### Step 3: Configure your ESPHome device with YAML
+
+Create an ESPHome YAML configuration with the following sections:
+ * `esphome: includes: [esphome-midea-ac]`
+ * ESP8266 only: `logger: baud_rate: 0` - disable serial port logging on the
+   sole ESP8266 hardware UART
+ * `switch: - switch component for manual control and feedback`
+ * `sensor: - temperature sensor component for automatic control`
+ * `climate: - climate component ащк setting the automatic operation of the air conditioner `
+ 
+All climate control actions are as follows:
+```yaml
+  - homeassistant.service:
+      service:  esphome.esp_conditioner_state
+      data: 
+        speed: "10" # fan speed (0-100)
+        mode: "-1" # mode in (auto=1,cool=2,dry=3,heat=4,fan=5)
+        power: "-1" # power control (off=0,on=1)
+        vswing: "-1" # vertical swing activation (off=0,on=1)
+        hswing: "-1" # horizontal swing activation (off=0,on=1)
+        temp: "-1" # temperature setpoint (17-30)
+```
+If you write "-1" in the parameter, it means that the parameter does not change.
+
+The temperature sensor and the switch component are written to the lambda function:
+```yaml
+custom_component:
+- lambda: |-
+    auto midea_climate = new Midea(id(uart_bus),id(temperature_sensor),id(power_switch));
+    return {midea_climate};
+```
+
+# Example configuration
+
+Below is an example configuration which will include wireless strength
+indicators and permit over the air updates. You'll need to create a
+`secrets.yaml` file inside of your `esphome` directory with entries for the
+various items prefixed with `!secret`.
+
+```yaml
+esphome:
+  name: esp_conditioner
+  platform: ESP8266
+  board: esp12e
+  includes:
+  - esphome-midea-ac
+
+wifi:
+  ssid: !secret wifi_secret_ssid
+  password: !secret wifi_secret_password
+
+  # Enable fallback hotspot (captive portal) in case wifi connection fails
+  ap:
+    ssid: "Midea Fallback Hotspot"
+    password: !secret wifi_secret_fallback_password
+
+captive_portal:
+
+# Enable logging
+logger:
+  baud_rate: 0
+  
+# Enable Home Assistant API
+api:
+  password: !secret hass_api_password
+
+ota:
+  password: !secret hass_ota_password
+
+uart:
+  id: uart_bus
+  tx_pin: GPIO1
+  rx_pin: GPIO3
+  baud_rate: 9600
+  
+sensor:
+- platform: template
+  id: temperature_sensor
+  
+climate:
+- platform: thermostat
+  name: "Thermostat Climate Controller"
+  sensor: temperature_sensor
+  
+  default_target_temperature_low: 20 °C
+  default_target_temperature_high: 25 °C
+  fan_mode_low_action:
+  - homeassistant.service:
+      service:  esphome.esp_conditioner_state
+      data: 
+        speed: "10"
+        mode: "-1"
+        power: "-1"
+        vswing: "-1"
+        hswing: "-1"
+        temp: "-1"
+        
+  fan_mode_medium_action:
+  - homeassistant.service:
+      service:  esphome.esp_conditioner_state
+      data: 
+        speed: "25"
+        mode: "-1"
+        power: "-1"
+        vswing: "-1"
+        hswing: "-1"
+        temp: "-1"
+        
+  fan_mode_high_action:
+  - homeassistant.service:
+      service:  esphome.esp_conditioner_state
+      data: 
+        speed: "100"
+        mode: "-1"
+        power: "-1"
+        vswing: "-1"
+        hswing: "-1"
+        temp: "-1"
+        
+  fan_mode_middle_action:
+  - homeassistant.service:
+      service:  esphome.esp_conditioner_state
+      data: 
+        speed: "50"
+        mode: "-1"
+        power: "-1"
+        vswing: "-1"
+        hswing: "-1"
+        temp: "-1"
+        
+  swing_vertical_action:
+  - homeassistant.service:
+      service:  esphome.esp_conditioner_state
+      data: 
+        vswing: "1"
+        mode: "-1"
+        power: "-1"
+        speed: "-1"
+        hswing: "-1"
+        temp: "-1"
+        
+  swing_off_action:
+  - homeassistant.service:
+      service:  esphome.esp_conditioner_state
+      data: 
+        vswing: "0"
+        mode: "-1"
+        power: "-1"
+        speed: "-1"
+        hswing: "-1"
+        temp: "-1"
+        
+  fan_only_action :
+  - homeassistant.service:
+      service:  esphome.esp_conditioner_state
+      data: 
+        mode: "5"
+        temp: "-1"
+        power: "1"
+        speed: "-1"
+        vswing: "-1"
+        hswing: "-1"
+        
+  dry_action:
+  - homeassistant.service:
+      service:  esphome.esp_conditioner_state
+      data: 
+        mode: "3"
+        temp: "23"
+        power: "1"
+        speed: "-1"
+        vswing: "-1"
+        hswing: "-1"
+
+  cool_action:
+  - homeassistant.service:
+      service:  esphome.esp_conditioner_state
+      data: 
+        mode: "2"
+        temp: "17"
+        power: "1"
+        speed: "-1"
+        vswing: "-1"
+        hswing: "-1"
+
+  heat_action:
+  - homeassistant.service:
+      service:  esphome.esp_conditioner_state
+      data: 
+        mode: "4"
+        temp: "30"
+        power: "1"
+        speed: "-1"
+        vswing: "-1"
+        hswing: "-1"
+
+  idle_action:
+  - homeassistant.service:
+      service:  esphome.esp_conditioner_state
+      data: 
+        power: "0"
+        mode: "-1"
+        temp: "-1"
+        speed: "-1"
+        vswing: "-1"
+        hswing: "-1"
+  
+switch:
+- platform: template
+  id: power_switch
+  name: "Conditioner Power Switch"
+
+  turn_on_action:
+  - homeassistant.service:
+      service:  esphome.esp_conditioner_state
+      data: 
+        power: "1"
+        mode: "1"
+        temp: "23"
+        speed: "102"
+        vswing: "0"
+        hswing: "0"
+  turn_off_action:
+  - homeassistant.service:
+      service:  esphome.esp_conditioner_state
+      data: 
+        power: "0"
+        mode: "-1"
+        temp: "-1"
+        speed: "-1"
+        vswing: "-1"
+        hswing: "-1"
+
+custom_component:
+- lambda: |-
+    auto midea_climate = new Midea(id(uart_bus),id(temperature_sensor),id(power_switch));
+    return {midea_climate};
+```
+
+# See Also
+
+## References and sources
+The knowledge to create the logic to monitor and control the Midea-'like' AC 
+units was gathered by reverse engineering Android applications from various 
+vendors, analyzing the UART protocol between an Artel unit and an SK103 SmartKey,
+documents found on the Internet, primarily in Chinese and the work from:
+
+* Mac Zhou: <https://github.com/mac-zhou/midea-msmart>
+* NeoAcheron: <https://github.com/NeoAcheron/midea-ac-py>
+* Rene Klootwijk: <https://github.com/reneklootwijk/node-mideahvac>
+* Sören Beye: <https://github.com/Hypfer/esp8266-midea-dehumidifier>
---
+++ midea.cpp
@@ -0,0 +1,278 @@
+// -*- coding: utf-8 -*-
+//
+// EspHome Midea Air Conditioner cpp
+//
+// Copyright 2020-2020 uncle-yura uncle-yura@tuta.io
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with this program; if not, write to the Free Software Foundation, Inc.,
+// 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+//
+
+
+#include "midea.h"
+using namespace esphome;
+
+// Public Methods //////////////////////////////////////////////////////////////
+
+void Midea::on_state(int mode, int temp, int power, int speed, int hswing, int vswing) {
+    #ifdef DEBUG
+        ESP_LOGD("Debug", "State changed: %d %d %d %d %d %d",mode, temp, power, speed, hswing, vswing);
+    #endif
+    set_status(mode, temp, power, speed, hswing, vswing);
+}
+
+void Midea::setup() {
+    register_service(&Midea::on_state, "state",{"mode", "temp", "power", "speed", "hswing", "vswing"});
+    this->set_update_interval(1000);
+}
+
+void Midea::update() {
+    if(queue_pointer_first<0)get_status();
+    
+    serial_buffer *current;
+    current=fifo_get();
+    
+    switch(current->status) {
+        case 0: //first attempt
+        case 4: //resend after 3 seconds
+        case 7: //resend after 6 seconds
+            write_array(current->buffer,current->size);
+            current->status++;
+            log_data(current);
+            break;
+        case 10: //pop message
+            fifo_pop();
+            break;
+        default:
+            current->status++;
+            break;
+    }
+}
+
+void Midea::loop() {
+    if (available()) {
+    char c = read();
+    
+    switch(c) {
+        case 0xAA:
+            receive_data.size = 0;
+            receive_data.buffer[receive_data.size] = c;
+            receive_data.size ++;
+            break;
+        
+        default:
+            if(receive_data.size>0 && receive_data.buffer[receive_data.size-1]==0xAA) {
+                receive_data.buffer[receive_data.size] = c;
+                readBytes(receive_data.buffer+2, c-1);
+                receive_data.size = c+1;
+                
+                log_data(&receive_data);
+                
+                parse_answer();
+            }
+            break;
+        }
+    }
+}
+
+
+// Private Methods //////////////////////////////////////////////////////////////
+
+
+void Midea::fifo_put(serial_buffer *packet) {
+    if(queue_pointer_first>=0) {
+        queue_pointer_last++;
+        queue_pointer_last &= 0x0F;
+        memcpy(&transmite_queue[queue_pointer_last],packet,sizeof(serial_buffer));
+    }
+    else {
+        memcpy(&transmite_queue[0],packet,sizeof(serial_buffer));
+        queue_pointer_first = 0;
+      queue_pointer_last = 0;
+    }
+}
+
+serial_buffer *Midea::fifo_get() {
+    if(queue_pointer_first>0)return &transmite_queue[queue_pointer_first];
+    else return &transmite_queue[0];
+}
+
+void Midea::fifo_pop() {
+    memset(&transmite_queue[queue_pointer_first],0,sizeof(serial_buffer));
+    if(queue_pointer_first==queue_pointer_last) {
+        queue_pointer_first = -1;
+        queue_pointer_last = -1;
+    }
+    else {
+        queue_pointer_first++;
+        queue_pointer_first &= 0x0F;
+    }
+}
+
+byte Midea::calculate_crc (byte *data, int length) {
+    byte crc = 0;
+    for (int i = 0; i < length; i++) {
+        crc = crc8Table[crc ^ data[i]];
+    }
+    return crc;
+}
+
+byte Midea::calculate_checksum (byte *data, int length) {
+    byte sum = 0;
+    for (int i = 0; i < length; i++) {
+        sum += data[i];
+    }
+    return 256 - (sum % 256);
+}
+
+void Midea::array_to_string(byte array[], unsigned int len, char buffer[]) {
+    for (unsigned int i = 0; i < len; i++) {
+        byte nib1 = (array[i] >> 4) & 0x0F;
+        byte nib2 = (array[i] >> 0) & 0x0F;
+        buffer[i*2+0] = nib1  < 0xA ? '0' + nib1  : 'A' + nib1  - 0xA;
+        buffer[i*2+1] = nib2  < 0xA ? '0' + nib2  : 'A' + nib2  - 0xA;
+    }
+    buffer[len*2] = '\0';
+}
+
+void Midea::clear_buffer(serial_buffer *buf) {
+    memset(buf->buffer, 0, sizeof(buf->buffer));
+    buf->size=0;
+}
+
+bool Midea::ckeck_crc(serial_buffer *buf) {
+    if(calculate_crc(buf->buffer + 10, buf->size-11)==0x00)
+        return true;
+    else
+        return false;
+}
+
+void Midea::set_crc(serial_buffer *buf) {
+    buf->buffer[buf->size] = calculate_crc(buf->buffer + 10, buf->size-10);
+    buf->size++;
+}
+
+bool Midea::ckeck_checksum(serial_buffer *buf) {
+    if(calculate_checksum(buf->buffer + 1, buf->size-1)==0x00)
+        return true;
+    else
+        return false;
+}
+
+void Midea::set_checksum(serial_buffer *buf) {
+    buf->buffer[buf->size] = calculate_checksum(buf->buffer + 1, buf->size);
+    buf->size++;
+}
+
+void Midea::log_data(serial_buffer *buf)
+{
+    #ifdef DEBUG
+        char str[512] = "";
+        array_to_string(buf->buffer, buf->size, str);
+        ESP_LOGD("Debug", "Packet(%c, time:%ds): %s",buf->type, buf->status, str);
+    #endif
+}
+
+void Midea::parse_answer() {
+    if(ckeck_checksum(&receive_data) && ckeck_crc(&receive_data)) {
+        if(fifo_get()->buffer[32] == receive_data.buffer[32]) fifo_pop();
+        
+        if(receive_data.buffer[10]==0xC0) {
+            status.state = (receive_data.buffer[11] & 0x01) > 0;
+            status.setpoint = (receive_data.buffer[12] & 0x0F) + 16 + ((receive_data.buffer[12] & 0x10) >> 4) * 0.5;
+            status.mode = (receive_data.buffer[12] & 0xE0) >> 5;
+            status.temp = (receive_data.buffer[21] - 50) / 2;
+            
+            if ((receive_data.buffer[13] & 0x7F) < 21) status.speed = 20;
+            else if ((receive_data.buffer[13] & 0x7F) < 41) status.speed = 40;
+            else if ((receive_data.buffer[13] & 0x7F) < 61) status.speed = 60;
+            else if ((receive_data.buffer[13] & 0x7F) < 101) status.speed = 80;
+            else status.speed = receive_data.buffer[13] & 0x7F;
+            
+            status.vswing = (receive_data.buffer[17] & 0x03) > 0;
+            status.hswing = (receive_data.buffer[17] & 0x0C) > 0;
+            
+            if(pswitch->state != status.state) {
+                pswitch->publish_state(status.state);
+            }
+            
+            if(tsensor->state != status.temp) {
+                tsensor->publish_state(status.temp);
+            }
+        }
+    }
+    clear_buffer(&receive_data);
+}
+
+void Midea::set_status(int mode, int temp, int power, int speed, int hswing, int vswing)
+{
+    serial_buffer transmite_data = {{0},0,'Q',0};
+    
+    clear_buffer(&transmite_data);
+    
+    memcpy(transmite_data.buffer, header_part, sizeof(header_part));
+    transmite_data.size = sizeof(header_part);
+    
+    memcpy(transmite_data.buffer + sizeof(header_part), set_status_part, sizeof(set_status_part));
+    transmite_data.size += sizeof(set_status_part);
+
+    // Set packet length
+    transmite_data.buffer[1] = transmite_data.size+1;
+    // Change header mode to SET
+    transmite_data.buffer[9] = 0x02;
+    
+    transmite_data.buffer[11] = 0x42 | (power<0 ? status.state : power);
+    transmite_data.buffer[12] = (mode<0 ? status.mode << 5 : mode <<5) | (temp<0 ? status.setpoint-16 : temp - 16);
+    transmite_data.buffer[13] = (speed<0 ? status.speed : speed);
+    transmite_data.buffer[17] = 0x30 | (hswing<0 ? status.hswing ? 0x0C : 0x00 : hswing) | (vswing<0 ? status.vswing ? 0x03 : 0x00 : vswing);
+    // Set control byte
+    transmite_data.buffer[32] = millis()/1000;
+
+    set_crc(&transmite_data);
+    set_checksum(&transmite_data);
+    
+    transmite_data.status = 0;
+    
+    fifo_put(&transmite_data);
+}
+
+void Midea::get_status()
+{
+    serial_buffer transmite_data = {{0},0,'Q',0};
+    clear_buffer(&transmite_data);
+
+    // Copy header part    
+    memcpy(transmite_data.buffer, header_part, sizeof(header_part));
+    transmite_data.size = sizeof(header_part);
+    
+    // Copy get status part
+    memcpy(transmite_data.buffer + sizeof(header_part), get_status_part, sizeof(get_status_part));
+    transmite_data.size += sizeof(get_status_part);
+    
+    // Set packet length
+    transmite_data.buffer[1] = transmite_data.size+1; 
+    
+    // Change header mode to GET
+    transmite_data.buffer[9] = 0x03;
+    
+    // Set control byte
+    transmite_data.buffer[30] = millis()/1000;
+    
+    set_crc(&transmite_data);
+    set_checksum(&transmite_data);
+    
+    transmite_data.status = 0;
+    
+    fifo_put(&transmite_data);
+}
